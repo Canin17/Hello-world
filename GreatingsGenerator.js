c2756a88aa9c9376900f6c7d463224a0cf898fb8
canin17 <firepower.sk@hotmail.com>
2020-11-23 17:58:28 -0600

    created a randomized  console output, fixed a bug

--- GreatingsGenerator.js
+++ GreatingsGenerator.js
@@ -1,10 +1,10 @@
 // you may add and subtract strings from the arrays
 const greetingArr = ["Hello there","Welcome","Hello","Eh, how ya doing", "Science"]
 const prefixArr = ["miss","mister","general","doctor"]
-const nameArr = ["Kanobi", "Grievous", "Marshal", "Winters", "white"]
+const nameArr = ["Kanobi", "Grievous", "Marshal", "Winters", "White"]
 //Constants that generate a random number based on array length
 const greetingRandomNum = Math.floor(Math.random() * greetingArr.length)
 const prefixRandomNum = Math.floor(Math.random() * prefixArr.length)
-const NameRandomNum = Math.floor(Math.random() * nameArr.length)
+const nameRandomNum = Math.floor(Math.random() * nameArr.length)
 
-console.log(`${greetingArr[]}`)
+console.log(`${greetingArr[greetingRandomNum]} ${prefixArr[prefixRandomNum]} ${nameArr[nameRandomNum]}`)
