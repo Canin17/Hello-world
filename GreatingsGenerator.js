8309a9e1fe0c92c1d7c2c0dc254faf951084e5a9
canin17 <firepower.sk@hotmail.com>
2020-11-23 17:52:10 -0600

    added radomization variables

--- GreatingsGenerator.js
+++ GreatingsGenerator.js
@@ -1,6 +1,10 @@
-
-const greetingArr = ["Hello there","Welcome","Hello","Eh, how ya doing"]
+// you may add and subtract strings from the arrays
+const greetingArr = ["Hello there","Welcome","Hello","Eh, how ya doing", "Science"]
 const prefixArr = ["miss","mister","general","doctor"]
-const NameArr = ["Kanobi", "Grievous", "Marshal", "Winters"]
+const nameArr = ["Kanobi", "Grievous", "Marshal", "Winters", "white"]
+//Constants that generate a random number based on array length
+const greetingRandomNum = Math.floor(Math.random() * greetingArr.length)
+const prefixRandomNum = Math.floor(Math.random() * prefixArr.length)
+const NameRandomNum = Math.floor(Math.random() * nameArr.length)
 
-
+console.log(`${greetingArr[]}`)
