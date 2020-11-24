259406f916b8f76bc98aa17b646209a3ecbf7639
canin17 <firepower.sk@hotmail.com>
2020-11-23 18:11:58 -0600

    Added punctuation

--- GreatingsGenerator.js
+++ GreatingsGenerator.js
@@ -1,10 +1,12 @@
 // you may add and subtract strings from the arrays
-const greetingArr = ["Hello there","Welcome","Hello","Eh, how ya doing", "Science"]
+const greetingArr = ["Hello there,","Welcome,","Hello,","Eh, how ya doing", "Science,"]
 const prefixArr = ["miss","mister","general","doctor"]
 const nameArr = ["Kanobi", "Grievous", "Marshal", "Winters", "White"]
+const puncArr = [".", "!", "?", "!!!!",]
 //Constants that generate a random number based on array length
 const greetingRandomNum = Math.floor(Math.random() * greetingArr.length)
 const prefixRandomNum = Math.floor(Math.random() * prefixArr.length)
 const nameRandomNum = Math.floor(Math.random() * nameArr.length)
-
-console.log(`${greetingArr[greetingRandomNum]} ${prefixArr[prefixRandomNum]} ${nameArr[nameRandomNum]}`)
+const puncRandomNum = Math.floor(Math.random() * puncArr.length)
+// Outputs a random string from each array in a specfic order to make a sentence.
+console.log(`${greetingArr[greetingRandomNum]} ${prefixArr[prefixRandomNum]} ${nameArr[nameRandomNum]}${puncArr[puncRandomNum]}`)
